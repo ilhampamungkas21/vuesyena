3c29322d345f3c2cce278e2d968922a29b9c8f2b
ilhampamungkas21 <ilhampamungkas14@gmail.com>
2022-11-27 18:52:06 +0700

    commit pertama

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+
+    <HeaderSyena/>
+    <HeroSyena/>
+    <WomenSyena/>
+    <InstaSyena/>
+    <PartnerSyena/>
+    <FooterSyena/>
+
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderSyena from '@/components/HeaderSyena.vue'
+import HeroSyena from '@/components/HeroSyena.vue'
+import WomenSyena from '@/components/WomenSyena.vue'
+import InstaSyena from '@/components/InstaSyena.vue'
+import PartnerSyena from '@/components/PartnerSyena.vue'
+import FooterSyena from '@/components/FooterSyena.vue'
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderSyena,
+    HeroSyena,
+    WomenSyena,
+    InstaSyena,
+    PartnerSyena,
+    FooterSyena,
+  
+  }
+}
+</script>
